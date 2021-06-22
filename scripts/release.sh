db639429ee47c656b266f9e2ad637f2d42aff5e3
Lucca Pessoa da Silva Matos <luccapsm@protonmail.com>
2021-06-22 02:13:45 -0300

    chore: change echo message

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,7 +3,7 @@
 echo "📝  Do you want gen a new release? [y/N]: "
 read DEFAULT
 if [[ $DEFAULT =~ ^([Yy])$ ]]; then
-  echo "🚨 Making the release - Please, export your GITLAB_TOKEN before run this script"
+  echo "🚨 Making the release - Please, export your GITHUB_TOKEN before run this script"
   make release-debug
   git add . && git commit -am "chore: bump version file"
   make release
